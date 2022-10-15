94ffcb594613e92ef58777eacfed993cb1d34dc3
truongnh3 <truongnh3@vng.com.vn>
2022-10-16 03:44:33 +0700

    add UI

---
+++ run.py
@@ -0,0 +1,23 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+from ui import Ui_MainWindow
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.main_win = QMainWindow()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self.main_win)
+
+    def show(self):
+        self.main_win.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
