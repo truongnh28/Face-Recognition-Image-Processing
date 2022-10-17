8cbe06cac48b2796c2fc7548b5f2ff65e75d5ed2
vietpham3 <viet.pham3@tiki.vn>
2022-10-18 02:39:06 +0700

    recognition

---
+++ src/faces-train.py
@@ -0,0 +1,57 @@
+import os
+from PIL import Image
+import numpy as np
+import cv2
+import pickle
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+print("base: ", BASE_DIR)
+image_dir = os.path.join(BASE_DIR, "images")
+face_cascade = cv2.CascadeClassifier("cascades/data/haarcascade_frontalface_alt2.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+current_id = 0
+label_ids = {}
+x_train = []
+y_labels = []
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpeg") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(root).replace(" ", "-").lower()
+            # print(label, path)
+            if label not in label_ids:
+                label_ids[label] = current_id
+                current_id += 1
+            id_ = label_ids[label]
+            # print(label_ids)
+
+            # y_label.append(label)
+            # x_train.append(path)
+            pil_image = Image.open(path).convert("L")
+            # size = [550, 550]
+            # final_image = pil_image.resize(size, Image.ANTIALIAS)
+            image_array = np.array(pil_image, "uint8")
+            # print(image_array)
+            faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+            # print('this is id1: ', id_)
+
+            for (x, y, w, h) in faces:
+                roi = image_array[y:y+h, x:x+w]
+                x_train.append(roi)
+                # print("this is id: ", id_)
+                y_labels.append(id_)
+
+# print("this")
+# print(x_train)
+# print("after this line")
+# print(y_labels)
+
+
+with open("labels.pickle", 'wb') as f:
+    pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("trainner.yml")
---
+++ src/faces.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2, sys
+import pickle
+
+labels = {}
+
+with open("labels.pickle", 'rb') as f:
+    ogLabels = pickle.load(f)
+    labels = {v: k for k,v in ogLabels.items()}
+
+
+# cap = cv2.VideoCapture(0)
+# prefix = '/Users/lap01685/PycharmProjects/pythonProject/venv/lib/python3.8/site-packages/'
+# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + '')
+face_cascade = cv2.CascadeClassifier("cascades/data/haarcascade_frontalface_alt2.xml")
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+
+def recognize(frame, x, y, roi_gray):
+    id_, conf = recognizer.predict(roi_gray)
+    print('conf: ', conf)
+    if 10 <= conf <= 85:
+        print(id_)
+        print(labels[id_])
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        name = labels[id_]
+        color = [255, 255, 255]
+        stroke = 2
+        cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+
+
+def detect_and_recog(prefix, photo_path):
+        #ret, frame = cap.read()
+    frame = cv2.imread(photo_path)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=4, minSize=(20, 20))
+    for (x, y, w, h) in faces:
+        # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        # print(x, y, w, h, '\n')
+        # print("here", gray, '\n')
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+        # print('roi_gay', roi_gray, '\n')
+
+        img_item = prefix + "images/result/my-image3.png"
+
+        color = (255, 0, 0)
+        stroke = 2
+        x_end_cord = x + w
+        y_end_cord = y + h
+        cv2.rectangle(frame, (x, y), (x_end_cord, y_end_cord), color, stroke)
+        print("here")
+        # recognizer
+        recognize(frame, x, y, roi_gray)
+        cv2.imwrite(img_item, frame)
+
+    cv2.imshow('frame', frame)
+
+
+    # cap.release()
+    cv2.destroyAllWindows()
+
+
+def main():
+    detect_and_recog('', "/Users/lap01685/ProjectForImgProcessing/Face-Recognition-Image-Processing/src/images/test_photo/mypic01.jpg")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
